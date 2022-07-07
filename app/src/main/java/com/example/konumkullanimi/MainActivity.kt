905f9d03df00eb8c71177a63fe511329c1eba41d
Mürsel <murselcetin51@gmail.com>
2022-07-07 23:38:40 +0300

    konum

--- app/src/main/java/com/example/konumkullanimi/MainActivity.kt
+++ app/src/main/java/com/example/konumkullanimi/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.konumkullanimi
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.konumkullanimi.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var izinKontrol = 0
+    private lateinit var flpc: FusedLocationProviderClient
+    private lateinit var locationTask: Task<Location>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        flpc = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.buttonKonumAl.setOnClickListener {
+            Log.e("aaaaa","213213123321")
+            izinKontrol = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            if (izinKontrol == PackageManager.PERMISSION_GRANTED) {
+                locationTask = flpc.lastLocation
+                konumBilgisiAl()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 100)
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        izinKontrol = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            locationTask = flpc.lastLocation
+            konumBilgisiAl()
+        }
+    }
+
+    fun konumBilgisiAl() {
+        locationTask.addOnSuccessListener {
+            if (it != null) {
+                binding.textViewKonum.text = "${it.latitude} - ${it.longitude}"
+            } else {
+                binding.textViewKonum.text = "Konum Alınamadı!!!"
+            }
+        }
     }
 }
